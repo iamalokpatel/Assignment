9a226a00178bc4278bbcfac9a33aee323e3b6665
Alok Patel <alokpatel23273@gmail.com>
2025-06-17 21:20:47 +0530

    Implemented USer Login Features

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,11 @@
-import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-// Admin: create user of any role
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    console.log("Incoming Data:", req.body); //
+    console.log("Incoming Data:", req.body);
 
     const exists = await User.findOne({ email });
     if (exists)
@@ -24,5 +24,31 @@ export const registerUser = async (req, res) => {
     res
       .status(500)
       .json({ message: "Failed to create user", error: error.message });
+  }
+};
+
+export const loginUSer = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      token,
+      user: { id: user._id, role: user.role, name: user.name },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed", error: error.message });
   }
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { registerUser } from "../controllers/authController.js";
+import { registerUser, loginUSer } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser); // For Normal User only
-
+router.post("/login", loginUSer);
 export default router;
